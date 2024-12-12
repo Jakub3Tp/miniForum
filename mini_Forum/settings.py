690d58a918d74a836fc6db6dc0219ba26979e3ed
student06 <jakub.gola2@teb.edu.pl>
2024-12-12 14:13:01 +0100

    12.12.2024_Jakub

--- mini_Forum/settings.py
+++ mini_Forum/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -104,12 +105,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'pl'
+TIME_ZONE = 'Europe/Warsaw'
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
