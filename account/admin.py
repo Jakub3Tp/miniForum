ead9a2bb8c7c46a5535c9de3c1efe563d205b8ef
dmitryL123 <154210427+dmitryL123@users.noreply.github.com>
2024-12-15 21:28:47 +0100

    15.12.2024_Dmitry

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Console
+from .models import Comment
+from .models import Rating
 
-# Register your models here.
+@admin.register(Console)
+class ConsoleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nazwa', 'producent', 'data_premiery', 'autor', 'data_utworzenia', 'data_aktualizacji')
+    search_fields = ('nazwa', 'producent')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'konsola', 'autor', 'tresc', 'data_utworzenia')
+    search_fields = ('konsola', 'autor')
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'konsola', 'autor', 'ocena', 'data_utworzenia')
+    search_fields = ('konsola', 'autor')
+    list_filter = ('ocena', 'konsola')
