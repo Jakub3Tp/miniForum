96c457d3fc8dc74a55b0faffa3d028315ad05d4a
dmitryL123 <154210427+dmitryL123@users.noreply.github.com>
2025-01-03 23:42:46 +0100

    03.01_Dmitry

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import register
 
 urlpatterns = [
     #path('login/', views.user_login, name='login')
@@ -9,5 +8,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
-    path('edit/', views.edit, name='edit')
-]
+    path('edit/', views.edit, name='edit'),
+    path('consoles/', views.console_list, name='console_list'),
+    path('consoles/add/', views.add_console, name='add_console'),
+    path('consoles/<int:pk>/edit/', views.edit_console, name='edit_console'),
+    path('consoles/<int:pk>/', views.console_detail, name='console_detail'),
+    path('consoles/<int:pk>/delete/', views.delete_console, name='delete_console'),
